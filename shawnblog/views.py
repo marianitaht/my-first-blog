eb6783b4fec5ec59334be94ca40d13a7465206f3
marianitaht <mjhernandeztzul2006@elvallecolegio.edu.gt>
2024-07-22 19:19:25 -0600

    Cambie el HTML para la página.

--- shawnblog/views.py
+++ shawnblog/views.py
@@ -1,15 +1,13 @@
 # shawnblog/views.py
 from django.shortcuts import render
 from .models import Post
-
 def post_list(request):
-    post_list = Post.objects.all()
-    return render(request, 'shawnblog/post_list.html', {'posts': post_list})
+    # Aquí debes tener lógica para obtener y pasar datos a la plantilla
+    return render(request, 'shawnblog/post_list.html')
 
 def bebidas_view(request):
-    post_list = Post.objects.all()  # Cambia 'Post' por el modelo que estés utilizando
-    return render(request, 'bebidas.html', {'posts': post_list})
-
+    # Aquí debes tener lógica para obtener y pasar datos a la plantilla
+    return render(request, 'shawnblog/bebidas.html')
 
 
 
