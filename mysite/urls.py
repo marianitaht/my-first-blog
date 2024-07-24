9fbd3610077dfe5b345f6750a4a8487fa6f0a52c
marianitaht <mjhernandeztzul2006@elvallecolegio.edu.gt>
2024-07-24 11:13:00 -0600

    Ahora si

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shawnblog.urls'))
+    path('bebidas/', include('shawnblog.urls')),
+    path('', include('shawnblog.urls')),
 ]
--- shawnblog/urls.py
+++ shawnblog/urls.py
@@ -5,6 +5,6 @@ from shawnblog import views  # Asegúrate de importar las vistas desde tu aplica
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', views.post_list, name='post_list'),
-    path('bebidas/', views.bebidas_view, name='bebidas'),  # Usa 'bebidas_view' en lugar de 'bebidas'
+    path('bebidas.html/', views.bebidas_view, name='bebidas'),  # Usa 'bebidas_view' en lugar de 'bebidas'
+    path('', views.post_list, name='post_list'),
 ]
